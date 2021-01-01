380e2b9ba2649b7ccdd65f91aca9c245beb16165
Amit0412 <59620280+Amit0412@users.noreply.github.com>
2021-01-02 00:01:35 +0530

    web crawl

---
+++ WEBPAGE CRAWL.js
@@ -0,0 +1,12 @@
+This is just the sample code.
+
+#Mative motive is to find word and match with all links.
+let vr=document.links;
+let rv="dfrf"
+for(var e=0;e<vr.length;e++)
+{
+if(vr[i].href.include("drdf")
+{
+console.log(vr[i].href)
+}
+}
